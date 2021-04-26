36cb50a2a111bdf13636ed5563ca80a2ff5b9061
Kaleem Sattar <kaleemsattar786@gmail.com>
2021-04-26 17:37:53 +0500

    background color changed

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     margin:20,
     alignItems:'center',
     justifyContent:'center',
